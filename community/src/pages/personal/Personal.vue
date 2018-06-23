6454a4691017351f24adce401ce72de16a2c3087
ppttfish <ppttfish@163.com>
2018-06-23 09:12:17 +0800

    adjust

--- community/src/pages/personal/Personal.vue
+++ community/src/pages/personal/Personal.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="personal-page">
     <p-header></p-header>
+    <info-banner></info-banner>
   </div>
 </template>
 
 <script>
 import PHeader from 'common/components/Header'
+import InfoBanner from './components/Banner'
 export default {
   name: 'Pesonal',
   components: {
-    PHeader
+    PHeader,
+    InfoBanner
   }
 }
 </script>
